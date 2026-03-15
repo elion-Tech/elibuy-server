76d014e0586910752b43645e751c71089ce8ecb6
elion-Tech <igweokwuekene@gmail.com>
2026-03-15 02:59:42 +0100

    Update authRoutes.ts

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import { signup, login } from '../controllers/authController.js';
+import { 
+  signup, 
+  login,
+  forgotPassword,
+  resetPassword,
+  getMe
+} from '../controllers/authController.js';
+import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.put('/reset-password/:token', resetPassword);
+router.get('/me', auth, getMe);
 
 export default router;
